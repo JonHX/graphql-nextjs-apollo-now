4c55dae5f97d73e38625d8fd27692f3df4875b82
Jon Tarrant <jon.tarrant@holidayextras.com>
2019-05-06 13:38:33 +0100

    checking now routing works correctly

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
 import App from '../components/App'
-import AllPeople from '../components/allPeople'
+import { People, Vehicles } from '../components/lists/'
 import React from 'react'
 import { withPageRouter } from '../lib/withPageRouter'
 
 class Main extends React.Component {
   render () {
-    console.log(this.props)
     return (
       <App>
         <h1>{this.props.router.query.title}</h1>
-        <AllPeople />
+        <People />
+        <Vehicles />
       </App>
     )
   }
--- pages/person.js
+++ pages/person.js
@@ -1,5 +1,4 @@
 import App from '../components/App'
-import AllPeople from '../components/allPeople'
 import React from 'react'
 import { withPageRouter } from '../lib/withPageRouter'
 
@@ -8,14 +7,9 @@ class Person extends React.Component {
     return (
       <App>
         <h1>{this.props.router.query.person}</h1>
-        <AllPeople />
       </App>
     )
   }
-}
-
-Person.getInitialProps = async ({ query }) => {
-  return { id: query.id }
 }
 
 export default withPageRouter(Person)
